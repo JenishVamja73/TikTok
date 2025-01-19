b10f6204756bbb1e28cbb9ddb9bd20a1219e491f
JenishVamja73 <jenishvamja091@gmail.com>
2025-01-19 16:44:48 +0530

    navgtion

---
+++ Screen/Navgtion/Index.tsx
@@ -0,0 +1,21 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import Login from '../Login/Index';
+const Stack = createNativeStackNavigator();
+
+
+
+const Navgtion = () => {
+  return (
+    <>
+     <NavigationContainer>
+      <Stack.Navigator initialRouteName="login">
+        <Stack.Screen name="login" component={Login} />
+
+      </Stack.Navigator>    
+      </NavigationContainer>
+      </>
+  );
+};
+export default Navgtion;
